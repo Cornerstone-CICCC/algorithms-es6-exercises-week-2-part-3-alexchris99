b937089abe07538daee135589ecb63e689afa211
Christian Jimenez <cajt@outlook.com>
2024-09-26 11:41:20 -0800

    Excercise 3

--- ex03.js
+++ ex03.js
@@ -1,9 +1,15 @@
 // Exercise 3: Set
-// Instructions: Create a Set named `uniqueNumbers` and add some numbers to it using .add(), including some duplicates. 
-// Verify that the Set only keeps unique values by logging it to the console.
+// Instructions: Create a Set named `uniqueNumbers` 
+//and add some numbers to it using .add(), including some duplicates. 
+// Verify that the Set only keeps unique values by 
+//logging it to the console.
 
 const uniqueNumbers = new Set();
 
 // Your code here
+
+for(let i = 0; i < 8; i++){
+    uniqueNumbers.add(Math.floor(Math.random()*4))
+}
 
 console.log(uniqueNumbers);
