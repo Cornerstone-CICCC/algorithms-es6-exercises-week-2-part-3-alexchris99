c1bda684a4cb3f972f3d0660b5abb51d276f6da5
Christian Jimenez <cajt@outlook.com>
2024-09-26 11:52:28 -0800

    Excercise 6

--- ex06.js
+++ ex06.js
@@ -1,12 +1,14 @@
 // Exercise 6: 'this' Keyword
-// Instructions: Create an object named `car` with a property `brand` and a method `logBrand` that logs the brand to the console. 
-// Use the `this` keyword inside the method to refer to the `brand` property.
+// Instructions: Create an object named `car` with a property
+//`brand` and a method `logBrand` that logs the brand to the console. 
+// Use the `this` keyword inside the method to refer to the 
+//`brand` property.
 
 const car = {
   brand: 'Toyota',
   logBrand() {
-    // Your code here
+    return this.brand
   }
 };
 
-car.logBrand();
+console.log(car.logBrand());
