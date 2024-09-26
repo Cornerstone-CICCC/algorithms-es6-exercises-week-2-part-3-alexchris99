5404ac0eed5ece889acbd1c710483f30c1039831
Christian Jimenez <cajt@outlook.com>
2024-09-26 11:48:01 -0800

    Excersice 5

--- ex04.js
+++ ex04.js
@@ -1,9 +1,16 @@
 // Exercise 4: Recursion
-// Instructions: Write a recursive function named `sumOfDigits` that calculates the sum of the elements of a given array. 
-// Test your function by calculating the sum of the array and logging the result to the console.
+// Instructions: Write a recursive function named 
+//`sumOfDigits` that calculates the sum of the elements 
+//of a given array. 
+// Test your function by calculating the sum of the array 
+//and logging the result to the console.
 
 function sumOfDigits(n) {
-  // Your code here
+  if(n.length != 1){
+    sumOfDigits(n.length-1){
+      
+    }
+  }
 }
 
 console.log(sumOfDigits([1, 3, 7, 1, 8])); // 20
